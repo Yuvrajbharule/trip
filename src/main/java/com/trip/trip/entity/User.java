e58df64c327c3d425106e93482e79ef2c8204906
Yuvrajbharule <113690977+Yuvrajbharule@users.noreply.github.com>
2025-02-01 23:17:10 +0530

    102 create entity User

---
+++ src/main/java/com/trip/trip/entity/User.java
@@ -0,0 +1,29 @@
+package com.trip.trip.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "role")
+    private String role;
+
+}
